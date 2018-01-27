2a4390054656041d7baa73936242e56015ad3b9d
Denis <denis.ievlev@gmail.com>
2018-01-27 03:52:25 +0200

    adding css files and more

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var app = function () {
   var express = require('express');
   var app = express();
 
+  app.use(express.static(__dirname + '/public'));
   app.set('views', __dirname + '/views');
   app.engine('html', require('ejs').renderFile);
   app.get('/', function(req, res) {
